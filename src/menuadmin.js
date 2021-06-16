ff41922ffc5915b0c0c7039c00f773ed02d03736
cuscuz-itz666 <78763402+cuscuz-itz666@users.noreply.github.com>
2021-06-16 14:44:03 -0300

    Update menuadmin.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -2,29 +2,29 @@ const menuadmin = (prefix, pushname) => {
  return `oiin aqui e o menu dos admins
  _obs para o bot executar esses comandos e nescessario dar adm para o bot_
  
- 😳 *COMANDO DOS ADEME*
+ 🌈 *COMANDO DOS ADEME*
  │
- ├─ 🐉 ${prefix}opengc
- ├─ 🐉 ${prefix}closegc
- ├─ 🐉 ${prefix}promote
- ├─ 🐉 ${prefix}demote
- ├─ 🐉 ${prefix}tagall
- ├─ 🐉 ${prefix}tagall2
- ├─ 🐉 ${prefix}tagall3
- ├─ 🐉 ${prefix}tagall4
- ├─ 🐉 ${prefix}tagall5
- ├─ 🐉 ${prefix}add
- ├─ 🐉 ${prefix}kick
- ├─ 🐉 ${prefix}listadmins
- ├─ 🐉 ${prefix}linkgroup
- ├─ 🐉 ${prefix}leave
- ├─ 🐉 ${prefix}welcome
- ├─ 🐉 ${prefix}nsfw
- ├─ 🐉 ${prefix}leveling
- ├─ 🐉 ${prefix}level
- ├─ 🐉 ${prefix}delete
- ├─ 🐉 ${prefix}simih
- └─ 🐉 ${prefix}ownergroup
+ ├─ 🪐 ${prefix}opengc
+ ├─ 🪐 ${prefix}closegc
+ ├─ 🪐 ${prefix}promote
+ ├─ 🪐 ${prefix}demote
+ ├─ 🪐 ${prefix}tagall
+ ├─ 🪐 ${prefix}tagall2
+ ├─ 🪐 ${prefix}tagall3
+ ├─ 🪐 ${prefix}tagall4
+ ├─ 🪐 ${prefix}tagall5
+ ├─ 🪐 ${prefix}add
+ ├─ 🪐 ${prefix}kick
+ ├─ 🪐 ${prefix}listadmins
+ ├─ 🪐 ${prefix}linkgroup
+ ├─ 🪐 ${prefix}leave
+ ├─ 🪐 ${prefix}welcome
+ ├─ 🪐 ${prefix}nsfw
+ ├─ 🪐 ${prefix}leveling
+ ├─ 🪐 ${prefix}level
+ ├─ 🪐 ${prefix}delete
+ ├─ 🪐 ${prefix}simih
+ └─ 🪐 ${prefix}ownergroup
  `
 
 
